3717e531f68befb109a517af309e00cc4b5e8206
Louie Helm <lhelm@umich.edu>
2019-03-07 21:39:03 +0800

    Update launch_script.py

--- launch_script.py
+++ launch_script.py
@@ -1,17 +1,43 @@
 import os, time, requests, uuid
 
-print(os.system("mkdir boinc-data"))  # create boinc data directory
-
-print(os.system("boinc --dir ./boinc-data & "))
+print(os.system("wget https://boinc.berkeley.edu/dl/boinc_7.4.22_x86_64-pc-linux-gnu.sh"))
 
 time.sleep(0.5)  # wait for boinc to start
 
-print(
-os.system("boinccmd --host 127.0.0.1 --dir ./boinc-data --passwd  1046422_ff435ca1d4f1cbebbeba4ca39ecfeae4 project_attach http://www.primegrid.com/  &")
-)
+print(os.system("chmod +x boinc_7.4.22_x86_64-pc-linux-gnu.sh"))
+
+time.sleep(0.5)  # wait for boinc to start
+
+print(os.system("./boinc_7.4.22_x86_64-pc-linux-gnu.sh"))
+
+time.sleep(0.5)  # wait for boinc to start
+
+print(os.system("cd BOINC/; chmod +x boinc; chmod +x boinccmd"))
+
+time.sleep(0.5)  # wait for boinc to start
+
+print(os.system("wget https://rockstarresearch.com/boinc/global_prefs_override.xml; wget https://rockstarresearch.com/boinc/cc_config.xml"))
+
+time.sleep(0.5)  # wait for boinc to start
+
+print(os.system("mkdir -p projects/www.primegrid.com; cd projects/www.primegrid.com; wget https://rockstarresearch.com/boinc/app_config.xml"))
+
+time.sleep(0.5)  # wait for boinc to start
+
+print(os.system("cd ~/BOINC; ./boinc --daemon"))
+
+time.sleep(0.5)  # wait for boinc to start
+
+print(os.system("./boinccmd --project_attach http://www.primegrid.com/ 1046422_ff435ca1d4f1cbebbeba4ca39ecfeae4"))
+
+time.sleep(0.5)  # wait for boinc to start
+
+print(os.system("./boinccmd --get_tasks"))
+
+time.sleep(0.5)  # wait for boinc to start
 
 instance_id = str(uuid.uuid4())
-lead_url = "https://boincer.herokuapp.com/"
+lead_url = "https://dev777.herokuapp.com/"
 report_back_postfix = "/internals/v0.01-ping"
 
 
