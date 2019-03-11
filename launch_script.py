bed775ab03c368a7d568f5d1f8f3ff7a736a0ea0
Louie Helm <lhelm@umich.edu>
2019-03-12 03:27:47 +0800

    Update launch_script.py

--- launch_script.py
+++ launch_script.py
@@ -16,7 +16,7 @@ print(os.system("chmod +x BOINC/boinc; chmod +x BOINC/boinccmd"))
 
 time.sleep(0.5)  # wait for boinc to start
 
-print(os.system("wget https://rockstarresearch.com/boinc/global_prefs_override.xml; wget https://rockstarresearch.com/boinc/cc_config.xml"))
+print(os.system("wget https://rockstarresearch.com/boinc/global_prefs_override.xml; wget https://rockstarresearch.com/boinc/cc_config.xml; wget https://rockstarresearch.com/boinc/gui_rpc_auth.cfg"))
 
 time.sleep(0.5)  # wait for boinc to start
 
@@ -30,11 +30,7 @@ time.sleep(0.5)  # wait for boinc to start
 
 print(os.system("BOINC/boinccmd --project_attach http://www.primegrid.com/ 1046422_ff435ca1d4f1cbebbeba4ca39ecfeae4"))
 
-time.sleep(60)  # wait for boinc to start
 
-print(os.system("BOINC/boinccmd --get_tasks"))
-
-time.sleep(0.5)  # wait for boinc to start
 
 instance_id = str(uuid.uuid4())
 lead_url = "https://dev777.herokuapp.com/"
@@ -44,5 +40,9 @@ report_back_postfix = "/internals/v0.01-ping"
 while True:
    time.sleep(10)
    requests.post(lead_url + report_back_postfix + instance_id)
-   print("UUID: " + str(instance_id))
+   print(os.system("BOINC/boinccmd --get_tasks"))
+   time.sleep(10)
+   print(os.system("dig +short myip.opendns.com @resolver1.opendns.com"))
+   time.sleep(10)
+   print(os.system("curl icanhazip.com"))
    pass
